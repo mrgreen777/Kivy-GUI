84e73ab6fb39c5f71d4fcd0994b124ad76a09127
mrgreen777 <jaraa33ben@gmail.com>
2018-11-17 15:44:20 +0100

    All windows are working

--- gui_app.py
+++ gui_app.py
@@ -1,11 +1,20 @@
 
 from kivy.app import App 
-from kivy.uix.boxlayout import BoxLayout 
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen 
 from kivy.core.window import Window
-import webbrowser
-# 
+from kivy.properties import ObjectProperty
+
+from arithmetic import Arithmetic
+
+import webbrowser 
+
+
 class KivyTutorRoot(BoxLayout):
     """The Root of all widgets"""
+
+    # Creating an obj math_screen that is in .kv file
+    math_screen = ObjectProperty(None)
     def __init__(self, **kwargs):
         super(KivyTutorRoot, self).__init__(**kwargs)
         # List of previous screens
@@ -22,6 +31,10 @@ class KivyTutorRoot(BoxLayout):
         if next_screen == "about this app":
             # This code is telling manager to the make the screen about screen
             self.ids.kivy_screen_manager.current = "about_screen"
+        else:
+            # make the question_text whatever the name of previous-screen
+            self.math_screen.question_text.text = next_screen
+            self.ids.kivy_screen_manager.current = "math_screen"
 
     def on_Back_Btn(self):
         # Check if theris any screen to go back 2
@@ -31,6 +44,15 @@ class KivyTutorRoot(BoxLayout):
             return True
         # no more screen to go back to
         return False
+
+
+class MathScreen(Screen, Arithmetic):
+    """Widget that acts as screen and hold the math questions"""
+    def __init__(self, *args, **kwargs):
+        super(MathScreen, self).__init__(*args, **kwargs)
+            
+
+
 
 class KivyTutorApp(App):
     """The Main Application"""
