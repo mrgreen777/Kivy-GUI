ef67fe15fcdd931a9b7b2d359a88e0fb43014966
mrgreen777 <jaraa33ben@gmail.com>
2018-11-17 12:17:40 +0100

    Created the main window with Actionbar and Settings Screen

--- gui_app.py
+++ gui_app.py
@@ -1,3 +1,23 @@
 
 from kivy.app import App 
 from kivy.uix.boxlayout import BoxLayout 
+
+# 
+class KivyTutorRoot(BoxLayout):
+    """The Root of all widgets"""
+    def __init__(self, **kwargs):
+        super(KivyTutorRoot, self).__init__(**kwargs)
+
+
+class KivyTutorApp(App):
+    """The Main Application"""
+    def __init__(self, **kwargs):
+        super(KivyTutorApp, self).__init__(**kwargs)
+
+    def build(self):
+                return KivyTutorRoot()
+
+
+if __name__ == '__main__':
+            # the run function is inherited from App class
+            KivyTutorApp().run()        
